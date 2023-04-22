ce4281224194b0b19f7fe7d949b37b9ec5b8986f
Karan <karanupd12@gmail.com>
2023-04-22 15:04:52 +0530

    added more java files

---
+++ subs.java
@@ -0,0 +1,8 @@
+public class subs{
+    public static void main(String[] args) {
+        int a=5;
+        int b=14;
+        int c=a-b;
+        System.out.println("substraction is ="+c);
+    }  
+}
