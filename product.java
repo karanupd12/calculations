3222d4f72ba92faeb8766fe0ef0df0b7d2f6ff8e
karan Upadhyay <karanupd12@gmail.com>
2023-04-23 17:55:14 +0530

    new file committed

---
+++ product.java
@@ -0,0 +1,8 @@
+public class product{
+    public static void main(String[] args) {
+        int a=5;
+        int b=14;
+        int c=a*b;
+        System.out.println("product is ="+c);
+    }  
+}
